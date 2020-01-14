0c167fcdb5b4e34438c7e49629abd2931ffbc75b
Do-ho <hong7511059@koreatech.ac.kr>
2020-01-14 22:25:31 +0900

    Update Navigation Bar

--- ui/src/component/Navbar.js
+++ ui/src/component/Navbar.js
@@ -5,27 +5,33 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <div>
+      <div className="nav_background_color">
         <div className="navigation">
-          <div className="navigation_container1">
-            <ul>
+          <div className="navigation_container">
+            <Link to="/" className="link">
+              <h1 className="logo"> 한표 </h1>
+            </Link>
+          </div>
+          <div className="navigation_container">
               <Link to="/" className="link">
-                <li className="logo"> 한표 </li>
+                <h2> 시간표 짜기 </h2>
               </Link>
-              <Link to="/" className="link">
-                <li> 시간표 짜기 </li>
-              </Link>
+          </div>
+          <div className="navigation_container">
               <Link to="/review" className="link">
-                <li> 강의 후기 </li>
+                <h2> 강의 후기 </h2>
               </Link>
-              <li> 마이페이지 </li>
-            </ul>
           </div>
-          <div className="navigation_container2">
-            <h5 className="login_phrase"> 한표를 더 편리하게 이용하세요! </h5>
-            <button className="login_button">로 그 인</button>
-            <h5> 회 원 가 입 | 아 이 디 · 비 밀 번 호 찾 기 </h5>
+          <div className="navigation_container">
+              <h2> 마이페이지 </h2>
           </div>
+          <div className="navigation_container">
+                <div className="navigation_container">
+                  <h5 className="login_phrase"> 한표를 더 편리하게 이용하세요! </h5>
+                  <button className="login_button">로 그 인</button>
+                  <h5> 회 원 가 입 | 아 이 디 · 비 밀 번 호 찾 기 </h5>
+                </div>
+          </div>  
         </div>
       </div>
     );
--- ui/src/page/Timetable.js
+++ ui/src/page/Timetable.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class timetable extends Component {
   render() {
     return (
       <div>
-        <h1> timetable 입니다. </h1>
+        <div className='container'>
+          <div className='box' >
+            <div className='notice'>
+              <h3><img className = 'notice_icon' alt='#' src={require('../img/notice_icon.jpg')}/>공지사항</h3>
+            </div>
+          </div>
+          <div className='box' >
+            <h1> 오른쪽</h1>
+          </div>
+        </div>
       </div>
     );
   }
